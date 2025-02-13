3e0c3ce3e3f087a0925670676e4b560659e5e3d9
James Mallon <jamesmallondev@gmail.com>
2025-02-13 19:02:06 -0300

    adding Content-Negotiation XML and YAML media types support to the InfoController->info action

--- src/main/java/com/thoseop/api/corporation/http/InfoController.java
+++ src/main/java/com/thoseop/api/corporation/http/InfoController.java
@@ -1,21 +1,45 @@
 package com.thoseop.api.corporation.http;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/corporation/v1")
 public class InfoController {
 
+    static final String _APPLICATION_YAML_VALUE = "application/x-yaml";
+
     @Value("${spring.application.name}")
     private String appName;
     
-    @GetMapping(value = {"", "/", "/info", "/info/"})
-    public ResponseEntity<Model> info(Model model) {
+    /**
+       ------------- JSON --------------
+       curl -s -H 'Accept: application/json' -L -X GET 'http://localhost:8080/api/corporation/v1/info' | jq
+
+       -------------- XML --------------
+       curl -s -H 'Accept: application/xml' -L -X GET 'http://localhost:8080/api/corporation/v1/info' | xmllint --format -
+       
+       ------------- YAML --------------
+       curl -s -H 'Accept: application/x-yaml' -L -X GET 'http://localhost:8080/api/corporation/v1/info' | yq
+
+       ------------- CORS --------------
+       curl -s -H 'Accept: application/json' -H 'Origin: http://localhost:3000' \
+       	-L -X GET 'http://localhost:8080/api/corporation/v1/info' | jq
+        
+     * @param model
+     * @return
+     */
+    @GetMapping(value = {"", "/", "/info", "/info/"},
+    		produces = { MediaType.APPLICATION_JSON_VALUE, 
+    			MediaType.APPLICATION_XML_VALUE, 
+    			_APPLICATION_YAML_VALUE })
+    public @ResponseBody ResponseEntity<Model> info(Model model) {
 	
 	model.addAttribute("Application", appName);
 	model.addAttribute("Description", "A Spring Boot 3 RESTfull API sample");
