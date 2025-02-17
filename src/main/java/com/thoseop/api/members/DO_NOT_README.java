148be12090d0b378b2d8bfa3dc9aaec2e875e76d
James Mallon <jamesmallondev@gmail.com>
2025-02-16 22:53:45 -0300

    saucing it...

---
+++ src/main/java/com/thoseop/api/members/DO_NOT_README.java
@@ -0,0 +1,11 @@
+package com.thoseop.api.members;
+
+public class DO_NOT_README {
+    /*
+   
+    
+    YOU HAVE BEEN TRAPPED IN THE INESCAPABLE NET OF RUIN BY YOUR OWN WANT OF SENSE. -- AESCHYLUS
+    
+    
+    */
+}
