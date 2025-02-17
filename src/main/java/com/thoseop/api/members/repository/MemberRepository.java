2e01a5b4855436b6c3fb2b1e883ef84eb393fea9
James Mallon <jamesmallondev@gmail.com>
2025-02-17 12:29:17 -0300

    adding query method findOneById() and setMemberStatus() to MemberRepository

--- src/main/java/com/thoseop/api/members/repository/MemberRepository.java
+++ src/main/java/com/thoseop/api/members/repository/MemberRepository.java
@@ -4,13 +4,28 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.thoseop.api.members.entity.MemberEntity;
 
+import jakarta.transaction.Transactional;
+
+@Transactional
 public interface MemberRepository extends CrudRepository<MemberEntity, Long> {
 
+    Optional<MemberEntity> findOneById(Long id);
     Optional<MemberEntity> findByEmail(String email);
-    
+
     Page<MemberEntity> findAll(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("""
+    	UPDATE MemberEntity m
+    	SET m.enabled = ?2
+    	WHERE m.id = ?1
+    		""")
+    int setMemberStatus(Long id, boolean enabled);
 }
