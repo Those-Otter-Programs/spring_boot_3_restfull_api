9ce9a503e1e4757313954aa7321747c7108c2b2c
James Mallon <jamesmallondev@gmail.com>
2025-02-16 22:56:22 -0300

    adding readMembers() and readMemberByEmail() to MemberService

--- src/main/java/com/thoseop/api/members/service/MemberService.java
+++ src/main/java/com/thoseop/api/members/service/MemberService.java
@@ -1,5 +1,9 @@
 package com.thoseop.api.members.service;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder.BCryptVersion;
 import org.springframework.stereotype.Service;
@@ -11,26 +15,59 @@ import com.thoseop.api.members.mapper.MemberMapper;
 import com.thoseop.api.members.repository.MemberRepository;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RequiredArgsConstructor
+@Slf4j
 @Service
-public class MemberService
-{
-
+public class MemberService {
+    
     private final MemberRepository memberRepository;
-    private final MemberMapper customerMapper;
-    //    private final PasswordEncoder passwordEncoder;
+    private final MemberMapper memberMapper;
 
-    public MemberResponse saveCustomer(MemberRequest request)
-    {
-        MemberEntity entity = this.customerMapper.mapRequestToEntity(request);
+    /**
+     * 
+     * @param request
+     * @return
+     */
+    public MemberResponse saveMember(MemberRequest request) {
+	log.info("Saving member");
+
+        MemberEntity entity = this.memberMapper.mapRequestToEntity(request);
 
         String hashPwd = new BCryptPasswordEncoder(BCryptVersion.$2B, 12).encode(entity.getPassword());
         entity.setPassword(hashPwd);
-//        entity.setRole("ROLE_%s".formatted(request.getCustomerRole()));
 
         MemberEntity savedEntity = memberRepository.save(entity);
 
-        return customerMapper.mapToResponse(savedEntity);
+        return memberMapper.mapToResponse(savedEntity);
+    }
+
+    /**
+     * 
+     * @param pageable
+     * @return
+     */
+    public Page<MemberResponse> readMembers(Pageable pageable) {
+	log.info("Reading all members - paginated");
+
+	Page<MemberEntity> membersList = memberRepository.findAll(pageable);
+	Page<MemberResponse> resultList = membersList.map(u -> memberMapper.mapToResponse(u));
+
+	return resultList;
+    }
+    
+    /**
+     * 
+     * @param email
+     * @return
+     */
+    public MemberResponse readMemberByEmail(String email) {
+	log.info("Reading member by email");
+
+	Optional<MemberEntity> result = this.memberRepository.findByEmail(email);
+	 return (result.isPresent())? 
+		 this.memberMapper.mapToResponse(result.get())
+		 : new MemberResponse();
     }
 }
