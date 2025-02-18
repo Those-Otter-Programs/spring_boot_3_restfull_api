721255177444dca8d9cce6e5af2e70725d315ef5
James Mallon <jamesmallondev@gmail.com>
2025-02-18 16:16:00 -0300

    adding OpenAPI annotations; updating memberAuthorities property type on MemberReponse

--- src/main/java/com/thoseop/api/members/http/response/MemberResponse.java
+++ src/main/java/com/thoseop/api/members/http/response/MemberResponse.java
@@ -2,12 +2,11 @@ package com.thoseop.api.members.http.response;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
+import java.util.List;
 
 import org.springframework.hateoas.RepresentationModel;
 
-import com.thoseop.api.members.entity.AuthorityEntity;
-
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -23,16 +22,31 @@ import lombok.experimental.Accessors;
 @NoArgsConstructor
 @EqualsAndHashCode(callSuper = false)
 @ToString
-public class MemberResponse extends RepresentationModel<MemberResponse> implements OtterApiResponse, Serializable {
+public class MemberResponse extends RepresentationModel<MemberResponse> implements Serializable {
 
     private static final long serialVersionUID = 7638859550206253144L;
 
+    @Schema( description = "The member's ID", example = "15")
     private Long memberId;
+    
+    @Schema(description = "The member's username", example = "John Wart")
     private String memberName;
+    
+    @Schema(description = "The member's email", example = "johnwart@corp.org")
     private String memberEmail;
+    
+    @Schema(description = "The member's mobile number", example = "(11) 91234-5678")
     private String memberMobileNumber;
+    
+    @Schema(description = "The member's status", example = "false")
     private boolean memberEnabled;
+    
+    @Schema(description = "The member's creation date", example = "1945-09-02")
     private Date memberCreatedAt;
+    
+    @Schema(description = "The member's last update date", example = "2025-02-18")
     private Date memberUpdatedAt;
-    private Set<AuthorityEntity> memberAuthorities;
+    
+    @Schema(description = "The member's authorities", example = "ROLE_ASSOCIATE, VIEWINFOCORP")
+    private List<String> memberAuthorities;
 }
