12c8bfc5c36a054ff6d8116221b4ab37887209ef
James Mallon <jamesmallondev@gmail.com>
2025-02-18 16:11:02 -0300

    improving JPA annotations attributes on AuthorityEntity

--- src/main/java/com/thoseop/api/members/entity/AuthorityEntity.java
+++ src/main/java/com/thoseop/api/members/entity/AuthorityEntity.java
@@ -1,28 +1,49 @@
 package com.thoseop.api.members.entity;
 
+import java.io.Serializable;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.experimental.Accessors;
 
+@Getter @Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
-@Getter 
-@Setter
 @Table(name="authorities")
-public class AuthorityEntity {
+public class AuthorityEntity implements Serializable {
+
+    private static final long serialVersionUID = 3354226995598969830L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private long id;
 
+    @Column(name = "name",
+	    nullable = false)
     private String name;
 
-    @ManyToOne
-    @JoinColumn(name="member_id")
+    @ManyToOne(
+	    fetch = FetchType.LAZY,
+            cascade = { 
+		CascadeType.DETACH, 
+		CascadeType.MERGE, 
+		CascadeType.PERSIST,
+		CascadeType.REFRESH })
+    @JoinColumn(name = "member_id")
     private MemberEntity member;
 }
