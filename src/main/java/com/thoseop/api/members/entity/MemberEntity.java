a81fc9f4af3f529ecb71c159a968a5ba652a43d9
James Mallon <jamesmallondev@gmail.com>
2025-02-18 16:10:12 -0300

    adding methods for authorities addition; improving JPA annotations attributes

--- src/main/java/com/thoseop/api/members/entity/MemberEntity.java
+++ src/main/java/com/thoseop/api/members/entity/MemberEntity.java
@@ -2,10 +2,12 @@ package com.thoseop.api.members.entity;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -22,12 +24,10 @@ import lombok.Setter;
 import lombok.ToString;
 import lombok.experimental.Accessors;
 
-
-@Getter
-@Setter
-@Accessors(chain = true)
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter @Setter
+@Accessors(chain = true)
 @EqualsAndHashCode
 @ToString
 @Entity
@@ -41,30 +41,87 @@ public class MemberEntity implements Serializable {
     @Column(name = "id")
     private Long id;
 
-    @Column(name = "name")
+    @Column(name = "name", 
+	    nullable = false)
     private String name;
 
-    @Column(name = "email")
+    @Column(name = "email", 
+	    nullable = false, 
+	    unique = true)
     private String email;
 
-    @Column(name = "mobile_number")
+    @Column(name = "mobile_number", 
+	    nullable = true)
     private String mobileNumber;
     
-    @Column(name = "password")
+    @Column(name = "password", 
+	    nullable = false)
     private String password;
     
-    @Column(name = "enabled")  
+    @Column(name = "enabled", 
+	    columnDefinition = "Boolean default false")  
     private Boolean enabled;
 
-    @Column(name = "created_at")
+    @Column(name = "created_at",
+	    columnDefinition = "Date default CURDATE()")
     @JsonIgnore
     private Date createdAt;
 
-    @Column(name = "updated_at")
+    @Column(name = "updated_at",
+	    columnDefinition = "Date default CURDATE()")
     @JsonIgnore
     private Date updatedAt;
 
-    @OneToMany(mappedBy = "member", fetch = FetchType.EAGER)
-    @JsonIgnore
+    @OneToMany(mappedBy = "member", 
+	    cascade = CascadeType.ALL, 
+//	    fetch = FetchType.LAZY)
+	    fetch = FetchType.EAGER)
     private Set<AuthorityEntity> authorities;
+
+    /**
+     * 
+     * @param authority
+     * @return
+     */
+    public MemberEntity addAuthority(AuthorityEntity authority) {
+	if (this.authorities == null)
+	    this.authorities = new HashSet<>();
+
+	authority.setMember(this);
+	this.authorities.add(authority);
+
+	return this;
+    }
+
+    /**
+     * 
+     * @param memberAuthorities
+     * @return
+     */
+    public MemberEntity addAuthorities(AuthorityEntity... memberAuthorities) {
+	if (this.authorities == null)
+	    this.authorities = new HashSet<>();
+
+	for (AuthorityEntity authority : memberAuthorities) {
+	    authority.setMember(this);
+	    this.authorities.add(authority);
+	}
+	return this;
+    }
+
+    /**
+     * 
+     * @param memberAuthorities
+     * @return
+     */
+    public MemberEntity addAuthorities(Set<AuthorityEntity> memberAuthorities) {
+	if (this.authorities == null)
+	    this.authorities = new HashSet<>();
+
+	for (AuthorityEntity authority : memberAuthorities) {
+	    authority.setMember(this);
+	    this.authorities.add(authority);
+	}
+	return this;
+    }
 }
