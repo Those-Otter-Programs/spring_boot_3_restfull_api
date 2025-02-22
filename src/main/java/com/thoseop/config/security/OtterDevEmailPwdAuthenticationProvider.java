2a403b8d7ceed549cf7e8405b5b424ecf0fdc96b
James Mallon <jamesmallondev@gmail.com>
2025-02-22 18:02:09 -0300

    removing unecessary imports from OtterDevEmailPwdAuthenticationProvider

--- src/main/java/com/thoseop/config/security/OtterDevEmailPwdAuthenticationProvider.java
+++ src/main/java/com/thoseop/config/security/OtterDevEmailPwdAuthenticationProvider.java
@@ -1,5 +1,4 @@
 package com.thoseop.config.security;
-
 //import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -7,7 +6,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
 
