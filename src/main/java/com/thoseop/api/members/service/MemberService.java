22f9eb99a59819f009b6e73daf5530264559c057
James Mallon <jamesmallondev@gmail.com>
2025-02-25 17:47:25 -0300

    MemberService - adding new authentication frelated properties to the saveMember() method; addding 'message' to the methods that return MemberResponse; adding readMemberDetilasByEmail(), changeMemberAccountExpiredStatus(), changeMemberCredentialsExpiredStatus(), changeMemberAccountLockedStatus(); modifying the method identifier changeMemberStatus() to changeMemberEnabledStatus()

--- src/main/java/com/thoseop/api/members/service/MemberService.java
+++ src/main/java/com/thoseop/api/members/service/MemberService.java
@@ -15,6 +15,8 @@ import com.thoseop.api.members.http.request.MemberCreateRequest;
 import com.thoseop.api.members.http.request.MemberManagePasswordRequest;
 import com.thoseop.api.members.http.request.MemberUpdatePasswordRequest;
 import com.thoseop.api.members.http.request.MemberUpdateRequest;
+import com.thoseop.api.members.http.response.MemberCreatedResponse;
+import com.thoseop.api.members.http.response.MemberDetailsResponse;
 import com.thoseop.api.members.http.response.MemberResponse;
 import com.thoseop.api.members.mapper.MemberMapper;
 import com.thoseop.api.members.repository.MemberRepository;
@@ -36,14 +38,17 @@ public class MemberService {
      * @param request
      * @return
      */
-    public MemberResponse saveMember(MemberCreateRequest request) {
+    public MemberCreatedResponse saveMember(MemberCreateRequest request) {
 	log.info("Saving member");
 
         MemberEntity entity = this.memberMapper.mapRequestToEntity(request);
 
         String hashPwd = this.passwordEncoder.encode(request.getMemberPassword());
         entity.setPassword(hashPwd);
-        entity.setEnabled(false)
+        entity.setAccountNotExpired(true)
+        	.setAccountNotLocked(true)
+        	.setCredentialsNotExpired(true)
+        	.setEnabled(false)
         	.setCreatedAt(new Date())
         	.setUpdatedAt(new Date());
 
@@ -52,7 +57,8 @@ public class MemberService {
 	
         MemberEntity savedEntity = memberRepository.save(entity);
 
-	return memberMapper.mapToResponse(savedEntity);
+        return memberMapper.mapToCreatedResponse(savedEntity)
+        	.setMessage("Member disabled by default. Account needs confirmation to get activated.");
     }
     
     /**
@@ -70,7 +76,8 @@ public class MemberService {
 
         MemberEntity updatedEntity = memberRepository.save(entity);
 
-        return memberMapper.mapToResponse(updatedEntity);
+        return memberMapper.mapToResponse(updatedEntity)
+        	.setMessage("Member updated");
     }
 
     /**
@@ -82,9 +89,24 @@ public class MemberService {
 	log.info("Reading all members - paginated");
 
 	Page<MemberEntity> membersList = memberRepository.findAll(pageable);
-	Page<MemberResponse> resultList = membersList.map(u -> memberMapper.mapToResponse(u));
+	Page<MemberResponse> resultList = membersList.map(u -> 
+	    memberMapper.mapToResponse(u).setMessage("Those Otter Programs by James Mallon")
+	);
 
 	return resultList;
+    }
+    
+    /**
+     * 
+     * @param email
+     * @return
+     */
+    public MemberDetailsResponse readMemberDetailsByEmail(String email) {
+	log.info("Reading member by email");
+
+	Optional<MemberEntity> result = this.memberRepository.findByEmail(email);
+
+	return this.memberMapper.mapToDetailsResponse(result.get());
     }
     
     /**
@@ -96,9 +118,8 @@ public class MemberService {
 	log.info("Reading member by email");
 
 	Optional<MemberEntity> result = this.memberRepository.findByEmail(email);
-	return (result.isPresent())? 
-		this.memberMapper.mapToResponse(result.get())
-		: new MemberResponse();
+
+	return this.memberMapper.mapToResponse(result.get());
     }
     
     /**
@@ -118,7 +139,8 @@ public class MemberService {
 	    entity = this.memberRepository.findByEmail(username).get();
 	}
 	
-	return this.memberMapper.mapToResponse(entity);
+	return this.memberMapper.mapToResponse(entity)
+		.setMessage("Password updated");
     }
     
     /**
@@ -137,7 +159,8 @@ public class MemberService {
 	    entity = this.memberRepository.findByEmail(request.getMemberUsername()).get();
 	}
 	
-	return this.memberMapper.mapToResponse(entity);
+	return this.memberMapper.mapToResponse(entity)
+		.setMessage("Password updated");
     }
     
     /**
@@ -145,16 +168,78 @@ public class MemberService {
      * @param id
      * @return
      */
-    public MemberResponse changeMemberStatus(Long id, MemberStatus status) {
+    public MemberResponse changeMemberAccountExpiredStatus(Long id, MemberStatus status) {
         log.info("Changing member id[{}] status to {}", id, status.getStatus());
 
-	int res = this.memberRepository.setMemberStatus(id, status.getStatus());
+	int res = this.memberRepository.setMemberAccountExpiredStatus(id, status.getStatus());
 
 	MemberEntity entity = null;
 	if (res == 1) {
 	    entity = this.memberRepository.findOneById(id).get();
 	}
 
-	return this.memberMapper.mapToResponse(entity);
+	return (status == MemberStatus.ENABLE) ?
+		this.memberMapper.mapToResponse(entity).setMessage("User's account not expired") :
+		this.memberMapper.mapToResponse(entity).setMessage("User's account expired");
+    }
+    
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    public MemberResponse changeMemberCredentialsExpiredStatus(Long id, MemberStatus status) {
+        log.info("Changing member id[{}] status to {}", id, status.getStatus());
+
+	int res = this.memberRepository.setMemberCredentialsExpiredStatus(id, status.getStatus());
+
+	MemberEntity entity = null;
+	if (res == 1) {
+	    entity = this.memberRepository.findOneById(id).get();
+	}
+
+	return (status == MemberStatus.ENABLE) ?
+		this.memberMapper.mapToResponse(entity).setMessage("User's credentials not expired") :
+		this.memberMapper.mapToResponse(entity).setMessage("User's credentials expired");
+    }
+    
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    public MemberResponse changeMemberAccountLockedStatus(Long id, MemberStatus status) {
+        log.info("Changing member id[{}] status to {}", id, status.getStatus());
+
+	int res = this.memberRepository.setMemberAccountLockedStatus(id, status.getStatus());
+
+	MemberEntity entity = null;
+	if (res == 1) {
+	    entity = this.memberRepository.findOneById(id).get();
+	}
+
+	return (status == MemberStatus.ENABLE) ?
+		this.memberMapper.mapToResponse(entity).setMessage("User's account was unlocked") :
+		this.memberMapper.mapToResponse(entity).setMessage("User's account was locked");
+    }
+    
+    /**
+     * 
+     * @param id
+     * @return
+     */
+    public MemberResponse changeMemberEnabledStatus(Long id, MemberStatus status) {
+        log.info("Changing member id[{}] status to {}", id, status.getStatus());
+
+	int res = this.memberRepository.setMemberEnabledStatus(id, status.getStatus());
+
+	MemberEntity entity = null;
+	if (res == 1) {
+	    entity = this.memberRepository.findOneById(id).get();
+	}
+
+	return (status == MemberStatus.ENABLE) ?
+		this.memberMapper.mapToResponse(entity).setMessage("User's account was enabled") :
+		this.memberMapper.mapToResponse(entity).setMessage("User's account was disabled");
     }
 }
