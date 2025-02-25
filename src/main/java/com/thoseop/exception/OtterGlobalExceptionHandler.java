503bfc1d06424b926763cd873387924d7037368c
James Mallon <jamesmallondev@gmail.com>
2025-02-25 17:17:15 -0300

    making global Authentication exceptions handlers more granular, by capturing AccountExpiredException, LockedException, CredentialsExpiredException, DisabledException, and BadCredentialsException separately

--- src/main/java/com/thoseop/exception/OtterGlobalExceptionHandler.java
+++ src/main/java/com/thoseop/exception/OtterGlobalExceptionHandler.java
@@ -4,9 +4,13 @@ import java.util.Date;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.AccountStatusException;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -17,48 +21,113 @@ import com.thoseop.exception.response.OtterAPIErrorResponse;
 @RestControllerAdvice
 public class OtterGlobalExceptionHandler {
 
-    @ExceptionHandler({ UsernameNotFoundException.class, BadCredentialsException.class })
+    @ExceptionHandler({ UsernameNotFoundException.class })
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public OtterAPIErrorResponse handleAuthenticationException(Exception ex) {
-	return new OtterAPIErrorResponse(new Date().toString(), 
+    public OtterAPIErrorResponse handleAuthenticationException(UsernameNotFoundException ex) {
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
 		HttpStatus.UNAUTHORIZED.value(),
-		"Error: %s".formatted(HttpStatus.UNAUTHORIZED.value()), 
-		ex.getMessage(), ex.getMessage());
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		"/token");
+    }
+
+    @ExceptionHandler({ AccountExpiredException.class })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public OtterAPIErrorResponse handleAuthenticationException(AccountExpiredException ex) {
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
+		HttpStatus.UNAUTHORIZED.value(),
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		"/token");
+    }
+
+    @ExceptionHandler({ LockedException.class })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public OtterAPIErrorResponse handleAuthenticationException(LockedException ex) {
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
+		HttpStatus.UNAUTHORIZED.value(),
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		"/token");
+    }
+
+    @ExceptionHandler({ CredentialsExpiredException.class })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public OtterAPIErrorResponse handleAuthenticationException(CredentialsExpiredException ex) {
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
+		HttpStatus.UNAUTHORIZED.value(),
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		"/token");
+    }
+
+    @ExceptionHandler({ DisabledException.class })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public OtterAPIErrorResponse handleAuthenticationException(DisabledException ex) {
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
+		HttpStatus.UNAUTHORIZED.value(),
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		"/token");
+    }
+
+    @ExceptionHandler({ BadCredentialsException.class })
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public OtterAPIErrorResponse handleAuthenticationException(BadCredentialsException ex) {
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
+		HttpStatus.UNAUTHORIZED.value(),
+		ex.getClass().getName(), 
+		ex.getCause().toString(), 
+		"/token");
     }
 
     @ExceptionHandler(InsufficientAuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public OtterAPIErrorResponse handleInsufficientAuthenticationException(InsufficientAuthenticationException ex) {
-	return new OtterAPIErrorResponse(new Date().toString(), 
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
 		HttpStatus.UNAUTHORIZED.value(),
-		"Error: %s".formatted(HttpStatus.UNAUTHORIZED.value()), 
-		ex.getMessage(), ex.getMessage());
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		ex.getMessage());
     }
 
     @ExceptionHandler(AccountStatusException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public OtterAPIErrorResponse handleAccountStatusException(AccountStatusException ex) {
-	return new OtterAPIErrorResponse(new Date().toString(), 
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
 		HttpStatus.UNAUTHORIZED.value(),
-		"Error: %s".formatted(HttpStatus.UNAUTHORIZED.value()), 
-		ex.getMessage(), ex.getMessage());
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		ex.getMessage());
     }
 
     @ExceptionHandler(AccessDeniedException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
     public OtterAPIErrorResponse handleAccessDeniedException(AccessDeniedException ex) {
-	return new OtterAPIErrorResponse(new Date().toString(), 
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
 		HttpStatus.FORBIDDEN.value(),
-		"Error: %s".formatted(HttpStatus.FORBIDDEN.value()), 
-		ex.getMessage(), ex.getMessage());
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		ex.getMessage());
     }
 
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public OtterAPIErrorResponse handleOtherException(Exception ex) {
-	return new OtterAPIErrorResponse(new Date().toString(), 
+	return new OtterAPIErrorResponse(
+		new Date().toString(), 
 		HttpStatus.INTERNAL_SERVER_ERROR.value(),
-		"Error: %s".formatted(HttpStatus.INTERNAL_SERVER_ERROR.value()), 
-		ex.getMessage(), ex.getMessage());
+		ex.getClass().getName(), 
+		ex.getMessage(), 
+		ex.getMessage());
     }
 }
