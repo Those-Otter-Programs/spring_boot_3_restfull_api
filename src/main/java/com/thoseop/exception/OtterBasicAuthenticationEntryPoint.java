06fe37194d87f8118868929a01df945a67da584c
James Mallon <jamesmallondev@gmail.com>
2025-02-25 17:11:52 -0300

    commenting redundant code section on OtterBasicAuthenticationEntryPoint

--- src/main/java/com/thoseop/exception/OtterBasicAuthenticationEntryPoint.java
+++ src/main/java/com/thoseop/exception/OtterBasicAuthenticationEntryPoint.java
@@ -1,7 +1,6 @@
 package com.thoseop.exception;
 
 import java.io.IOException;
-import java.util.Date;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
@@ -9,9 +8,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.thoseop.exception.response.OtterAPIErrorResponse;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -35,11 +31,11 @@ public class OtterBasicAuthenticationEntryPoint implements AuthenticationEntryPo
 	response.setHeader("Otter-Spring_Boot_3_RESTfull_API", "Custom Handling HTTP 401");
 	response.setStatus(HttpStatus.UNAUTHORIZED.value());
 	response.setContentType("application/json;charset=UTF-8");
-
-	OtterAPIErrorResponse errorResponse = new OtterAPIErrorResponse(new Date().toString(), response.getStatus(),
-		"Error: %s".formatted(response.getStatus()), authException.getMessage(), request.getServletPath());
-	ObjectMapper objMapper = new ObjectMapper();
-	response.getWriter().write(objMapper.writeValueAsString(errorResponse));
+	
+//	OtterAPIErrorResponse errorResponse = new OtterAPIErrorResponse(new Date().toString(), response.getStatus(),
+//		"Error: %s".formatted(response.getStatus()), authException.getMessage(), request.getServletPath());
+//	ObjectMapper objMapper = new ObjectMapper();
+//	response.getWriter().write(objMapper.writeValueAsString(errorResponse));
 
 	this.resolver.resolveException(request, response, null, authException);
     }
