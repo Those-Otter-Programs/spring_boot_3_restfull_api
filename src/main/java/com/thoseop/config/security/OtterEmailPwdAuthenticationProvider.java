bdb3b76ece2f4e3073fd23ff4bddcb9a8c64ecae
James Mallon <jamesmallondev@gmail.com>
2025-02-25 17:19:58 -0300

    throwing authentication exceptions separately in the OtterEmailPwdAuthenticationProvider in order to have more informative error responses

--- src/main/java/com/thoseop/config/security/OtterEmailPwdAuthenticationProvider.java
+++ src/main/java/com/thoseop/config/security/OtterEmailPwdAuthenticationProvider.java
@@ -1,7 +1,11 @@
 package com.thoseop.config.security;
 
+import org.springframework.security.authentication.AccountExpiredException;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.CredentialsExpiredException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -24,12 +28,27 @@ public class OtterEmailPwdAuthenticationProvider implements AuthenticationProvid
 	String username = authentication.getName();
 	String pwd = authentication.getCredentials().toString();
 
+	// CHECK IF USERNAME EXISTS AND THROWS UsernameNotFoundException IF NOT 
 	UserDetails userDetails = userDetailsService.loadUserByUsername(username);
 
-	if (passwordEncoder.matches(pwd, userDetails.getPassword()) && userDetails.isEnabled())
+	// CHECK IF ACCOUNT EXPIRED
+	if (!userDetails.isAccountNonExpired())
+	    throw new AccountExpiredException("User account is expired");
+	// CHECK IF ACCOUNT NON-LOCKED
+	if (!userDetails.isAccountNonLocked()) 
+	    throw new LockedException("User account is locked");
+	// CHECK IF CREDENTIALS NON-EXPIRED
+	if (!userDetails.isCredentialsNonExpired())
+	    throw new CredentialsExpiredException("User credentials are expired");
+	// CHECK IF ACCOUNT ENABLED
+	if (!userDetails.isEnabled())
+	    throw new DisabledException("User account is disabled");
+
+	// CHECK IF PASSWORD IS CORRECT
+	if (passwordEncoder.matches(pwd, userDetails.getPassword()))
 	    return new UsernamePasswordAuthenticationToken(username, pwd, userDetails.getAuthorities());
 	else 
-	    throw new BadCredentialsException("Invalid password or inactivated user.");
+	    throw new BadCredentialsException("User provided invalid password.");
     }
 
     @Override
