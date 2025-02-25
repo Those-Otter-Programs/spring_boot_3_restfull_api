0d70608c77a48c2680d89e14c6369a35a83a85e7
James Mallon <jamesmallondev@gmail.com>
2025-02-25 17:29:18 -0300

    creating new response DTO MemberCreatedResponse for using on MemberController->saveMember responses

---
+++ src/main/java/com/thoseop/api/members/http/response/MemberCreatedResponse.java
@@ -0,0 +1,45 @@
+package com.thoseop.api.members.http.response;
+
+import java.io.Serializable;
+import java.util.List;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+@ToString
+public class MemberCreatedResponse extends RepresentationModel<MemberCreatedResponse> implements Serializable {
+
+    private static final long serialVersionUID = 7638859550206253144L;
+
+    @Schema( description = "The member's ID", example = "15")
+    private Long memberId;
+    
+    @Schema(description = "The member's username", example = "John Wart")
+    private String memberName;
+    
+    @Schema(description = "The member's email", example = "johnwart@corp.org")
+    private String memberEmail;
+    
+    @Schema(description = "The member's mobile number", example = "(11) 91234-5678")
+    private String memberMobileNumber;
+    
+    @Schema(description = "The member's authorities", example = "ROLE_ASSOCIATE, VIEWINFOCORP")
+    private List<String> memberAuthorities;
+    
+    @Schema(description = "Message to member", example = "You need to confirm your account")
+    private String message;
+}
