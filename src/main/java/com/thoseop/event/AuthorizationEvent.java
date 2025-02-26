85a53e3d74644595e43bead9358dfc52c8048a16
James Mallon <jamesmallondev@gmail.com>
2025-02-25 23:52:54 -0300

    renaming AuthorizationEvents.java to AuthorizationEvent.java

--- src/main/java/com/thoseop/event/AuthorizationEvent.java
+++ src/main/java/com/thoseop/event/AuthorizationEvent.java
@@ -8,7 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Component
-public class AuthorizationEvents {
+public class AuthorizationEvent {
     //    @EventListener
     //    public void onSuccess(AuthorizationGrantedEvent<?> successEvent)
     //    {
