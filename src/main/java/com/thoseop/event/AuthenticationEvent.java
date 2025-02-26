98f50070eb7d3e2508237d03a9da901cb65400c0
James Mallon <jamesmallondev@gmail.com>
2025-02-25 23:51:43 -0300

    adding code to save members authentication failures to the AuthenticationEvent

--- src/main/java/com/thoseop/event/AuthenticationEvent.java
+++ src/main/java/com/thoseop/event/AuthenticationEvent.java
@@ -1,24 +1,49 @@
 package com.thoseop.event;
 
+import java.util.Date;
+
 import org.springframework.context.event.EventListener;
 import org.springframework.security.authentication.event.AbstractAuthenticationFailureEvent;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 
+import com.thoseop.api.members_logs.entity.AuthenticationFailureLogEntity;
+import com.thoseop.api.members_logs.entity.enums.AuthStatus;
+import com.thoseop.api.members_logs.service.AuthenticationFailureLogService;
+
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+@RequiredArgsConstructor
 @Component
 @Slf4j
-public class AuthenticationEvents {
+public class AuthenticationEvent {
+
+    private final AuthenticationFailureLogService authFailLogService;
 
     @EventListener
     public void onSuccess(AuthenticationSuccessEvent authSuccessEvent) {
-	log.info("Login successful for the user: {}", authSuccessEvent.getAuthentication().getName());
+	log.info("AuthenticationEvents - login successful for the user: {} on {}", 
+		authSuccessEvent.getAuthentication().getName(),		
+		new Date(authSuccessEvent.getTimestamp()).toString());
     }
 
     @EventListener
     public void onFailure(AbstractAuthenticationFailureEvent authFailureEvent) {
-	log.error("Login failure for the user: {}", authFailureEvent.getAuthentication().getName(),
-		authFailureEvent.getException().getMessage());
+	log.error("AuthenticationEvents - login failure for the user: {}. Exception: {} on {}", 
+		authFailureEvent.getAuthentication().getName(),
+		authFailureEvent.getException().getMessage(),
+		new Date(authFailureEvent.getTimestamp()).toString());
+
+	WebAuthenticationDetails authDetails = 
+		(WebAuthenticationDetails) authFailureEvent.getAuthentication().getDetails();
+
+	authFailLogService.saveAuthLog(new AuthenticationFailureLogEntity( 
+		authFailureEvent.getAuthentication().getName(),
+		AuthStatus.FAILURE,
+		authDetails.getRemoteAddress(),
+		authFailureEvent.getException().getMessage(),
+		new Date(authFailureEvent.getTimestamp())));
     }
 }
