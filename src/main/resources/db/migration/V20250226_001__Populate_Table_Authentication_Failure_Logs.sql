f6a92ae7a9dcea4da89cd6adfea0a59b6440f8e5
James Mallon <jamesmallondev@gmail.com>
2025-02-27 09:31:56 -0300

    adding migration to populate initial authentication failure logs

---
+++ src/main/resources/db/migration/V20250226_001__Populate_Table_Authentication_Failure_Logs.sql
@@ -0,0 +1,5 @@
+INSERT INTO authentication_failure_logs VALUES
+	(1, 'ayrton.senna@bravo.com', 'FAILURE', '0:00:0:0:0:0:1', 'User account is expired', '2025-02-26 19:32:13.158', '2025-02-26 19:32:13.159'),
+	(2, 'ayrton.senna@bravo.com', 'FAILURE', '0:0:0:0:0:0:0:1', 'User account is locked', '2025-02-26 19:32:26.025', '2025-02-26 19:32:26.026'),
+	(3, 'ayrton.senna@bravo.com', 'FAILURE', '0:0:0:0:0:0:0:1', 'User credentials are expired', '2025-02-26 19:32:36.316', '2025-02-26 19:32:36.317'),
+	(4, 'ayrton.senna@bravo.com', 'FAILURE', '0:0:0:0:0:0:0:1', 'User account is disabled', '2025-02-26 19:32:46.517', '2025-02-26 19:32:46.518');
