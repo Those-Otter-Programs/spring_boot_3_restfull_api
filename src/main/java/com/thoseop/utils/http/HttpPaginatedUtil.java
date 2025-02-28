d3aeb6f92d8a41bbc11f400faeb2238d8bcfb72a
James Mallon <jamesmallondev@gmail.com>
2025-02-28 08:11:10 -0300

    HttpPaginatedUtil - creating util class to process sortBy query param on paginated actions; it makes use of a prefix to not expose the entity logic, and check the value against the entity's properties to determine if it is a sortable data

---
+++ src/main/java/com/thoseop/utils/http/HttpPaginatedUtil.java
@@ -0,0 +1,44 @@
+package com.thoseop.utils.http;
+
+import org.springframework.stereotype.Service;
+
+import com.thoseop.exception.InvalidSortByException;
+
+@Service
+public class HttpPaginatedUtil {
+
+    /**
+     * Method created to filter a sortBy query param from a paginated action.
+     * The method expects a prefix, which is used to cover the Entity logic, then
+     * it verifies the entity object properties to see if the sortBy value 
+     * exists in the Entity, thus making sure it is a sortable data.
+     * 
+     * It throws a custom exception and avoids the PropertyReferenceException and
+     * the NoSuchFieldException.
+     * 
+     * @param sortBy
+     * @param prefix
+     * @param entity
+     * @return
+     * @throws Exception
+     */
+    public static String filteringSortBy(String sortBy, String prefix, Object entity) throws Exception {
+        if (sortBy.contains(prefix)) {
+            sortBy = sortBy.replace(prefix, "");
+            
+            char c[] = sortBy.toCharArray();
+//            c[0] = Character.toLowerCase(c[0]);
+            c[0] += 32;
+            sortBy = new String(c);
+        } else
+            throw new InvalidSortByException("received sortBy is not a sortable data".formatted(sortBy));
+       
+        try {
+            entity.getClass().getDeclaredField(sortBy);
+        } catch (NoSuchFieldException ex) { 
+            throw new InvalidSortByException("received sortBy is not a sortable data".formatted(sortBy));
+        }
+        
+        return sortBy;
+    }
+}
