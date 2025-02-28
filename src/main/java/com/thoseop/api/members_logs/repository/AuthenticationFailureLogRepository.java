00383630e140994b899fc7e8778f40e31b4c3590
James Mallon <jamesmallondev@gmail.com>
2025-02-28 19:47:20 -0300

    AuthenticationFailureLogRepository - adding Query method with DTO projection

--- src/main/java/com/thoseop/api/members_logs/repository/AuthenticationFailureLogRepository.java
+++ src/main/java/com/thoseop/api/members_logs/repository/AuthenticationFailureLogRepository.java
@@ -5,8 +5,10 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.thoseop.api.members_logs.entity.AuthenticationFailureLogEntity;
+import com.thoseop.api.members_logs.entity.dto.MemberDetailsAuthenticationDTO;
 
 import jakarta.transaction.Transactional;
 
@@ -16,4 +18,23 @@ public interface AuthenticationFailureLogRepository extends JpaRepository<Authen
     Optional<AuthenticationFailureLogEntity> findOneById(Long id);
     
     Page<AuthenticationFailureLogEntity> findAllByUsername(String username, Pageable pageable);
+    
+    @Query(value = """
+		    SELECT new com.thoseop.api.members_logs.entity.dto.MemberDetailsAuthenticationDTO(
+		    	me.id,
+		    	me.email,
+		    	me.accountNotExpired,
+		    	me.credentialsNotExpired,
+		    	me.accountNotLocked,
+		    	me.enabled,
+		    	afl.id,
+		    	afl.eventResult,
+		    	afl.remoteIpAddress,
+		    	afl.message,
+		    	afl.authTime) 
+		    FROM MemberEntity me
+		    LEFT JOIN AuthenticationFailureLogEntity afl ON afl.username = me.email
+		    WHERE afl.username = ?1 
+		    """) // FUNCIONA
+    Page<MemberDetailsAuthenticationDTO> findMemberAuthLogs(String username, Pageable pageable);
 }
